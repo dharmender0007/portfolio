c0c0e9734f0865234644462a464a12b112dce5a3
Dharmender Jangir <jangirdharmender333@gmail.com>
2025-07-14 12:06:44 +0530

    portfolio

---
+++ src/sections/Projects.tsx
@@ -0,0 +1,95 @@
+import AmazonClone from "@/assets/images/amazon-clone.png";
+import TiffineService from "@/assets/images/tiffin.png";
+import Image from "next/image";
+import CheckCircleIcon from "@/assets/icons/check-circle.svg";
+import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
+import { SectionHeader } from "@/components/SectionHeader";
+import { Card } from "@/components/Card";
+
+const portfolioProjects = [
+  {
+    company: "Amazon Clon",
+    year: "2025",
+    title: "Ecommerce Website",
+    results: [
+      { title: "Purchase anything you want" },
+      { title: "Increased user engagement by 60%" },
+      { title: "Enhanced platform performance" },
+    ],
+    link: "https://github.com/dharmender0007/Amazon-Clone",
+    image: AmazonClone,
+  },
+  {
+    company: "Taffine-Service",
+    year: "2024",
+    title: "Order Food ",
+    results: [
+      { title: "Implemented Shameless Experience" },
+      { title: "Enhanced loading speed by 40%" },
+      { title: "Seamless performance on all devices" },
+    ],
+    link: "https://github.com/dharmender0007/tiffin-service",
+    image: TiffineService,
+  },
+  
+];
+
+
+export const ProjectsSection = () => {
+  return (
+    <section className="pb-16 lg:py-24" id="projects">
+      <div className="container">
+        <SectionHeader eyebrow="Real-world Results" title="Featured Projects" description="See how I tranformed concepts into engaging digital experiences."/>
+        <div className="mt-10 md:mt-20 flex flex-col gap-20">
+          {portfolioProjects.map((project, projectIndex) => (
+            <Card
+              key={project.title}
+              className="px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20 sticky"
+              style={{
+                top: `calc(64px + ${projectIndex * 40}px)`
+              }}
+            >
+              <div className="lg:grid lg:grid-cols-2 lg:gap-16">
+                <div className="lg:pb-16">
+                  <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
+                    <span>{project.company}</span>
+                    <span>&bull;</span>
+                    <span>{project.year}</span>
+                  </div>
+                  <h3 className="font-serif text-2xl mt-2 md:mt-5 md:text-4xl">
+                    {project.title}
+                  </h3>
+                  <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
+                  <ul className="flex flex-col gap-4 mt-4 md:mt-5">
+                    {project.results.map((result, index) => (
+                      <li
+                        key={index}
+                        className="flex gap-2 text-sm md:text-base text-white/50"
+                      >
+                        <CheckCircleIcon className="size-5 md:size-6" />
+                        <span>{result.title}</span>
+                      </li>
+                    ))}
+                  </ul>
+                  <a href={project.link}>
+                    <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
+                      <span>Visit Live Site</span>
+                      <ArrowUpRightIcon className="size-4" />
+                    </button>
+                  </a>
+                </div>
+                <div className="relative">
+                  <Image
+                    src={project.image}
+                    alt={project.title}
+                    className="mt-8 -mb-4 md:-mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none rounded-xl"
+                  />
+                </div>
+              </div>
+            </Card>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
